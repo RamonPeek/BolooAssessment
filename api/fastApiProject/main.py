a46449cf1ce97530c72aa301ef5bb8aab1d50ea9
RamonPeek <ramonpeek@hotmail.com>
2024-07-10 13:09:10 +0200

    Added backend code for voting, fetching votes and active poll

--- api/fastApiProject/main.py
+++ api/fastApiProject/main.py
@@ -11,11 +11,61 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Poll:
+    def __init__(self, id, question, answerOptions):
+        self.id = id
+        self.question = question
+        self.answerOptions = answerOptions
 
+class Answer:
+    def __init__(self, id, text):
+        self.id = id
+        self.text = text
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+class Voter:
+    def __init__(self, pollId, firstName: str, lastName: str):
+        self.pollId = pollId
+        self.firstName = firstName
+        self.lastName = lastName
+
+votes = []
+polls = [
+    Poll(1, "What is your favourite music genre?", [
+        Answer(1, "Rock"),
+        Answer(2, "Pop"),
+        Answer(3, "Country")
+    ])
+]
+
+@app.get("/polls/active")
+async def getActivePoll():
+    return polls[0]
+
+@app.get("/polls/{pollId}/votes")
+async def getPollVotes(pollId: int):
+    if not pollId:
+        raise HTTPException(status_code=400, detail="PollId is required!")
+
+    #Usually you would query these votes as much as possible on the DB side (where pollId = pollId).
+    pollVotes = []
+    for vote in votes:
+        if(vote.pollId == pollId):
+            pollVotes.append(vote)
+
+    return pollVotes
+
+@app.post("/polls/{pollId}/vote/{firstName}/{lastName}") #Usually things such as firstName and lastName would be retrieved from an accountIdentifier retrieved from an auth token/identifier.
+async def vote(pollId: int, firstName: str, lastName: str):
+    if not pollId:
+        raise HTTPException(status_code=400, detail="PollId is required!")
+
+    if not firstName or not lastName:
+        raise HTTPException(status_code=400, detail="Firstname and lastname are required!")
+
+    if any(vote.firstName == firstName and vote.lastName == lastName for vote in votes):
+        raise HTTPException(status_code=400, detail="Already voted!")
+
+    vote = Voter(pollId, firstName, lastName)
+
+    votes.append(vote)
+    return vote
